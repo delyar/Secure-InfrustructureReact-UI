7a69ca06ce6d7db61528be80bd8083bd0e7f5a77
Delyar Tabatabai <t-dtabatabai@microsoft.com>
2022-11-23 16:51:34 -0800

    'logic'

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function Main() {
 
     const tenantID = "asuifhapiuhdfiwuhefpqwufhaishudf";
-    const subscription = "my subscription"
-    const selectedResourceGroup = "R2"
+
+    const [subscription, setSubscription] = useState();
+    const [resourceGroup, setRG] = useState();
+    const [resourceType, setType] = useState("asa");
+    const [resourceName, setName] = useState();
+    const [region, setRegion] = useState();
 
     function submitPressed() {
 
-        console.log("delyar")
+        console.log(tenantID, resourceGroup, subscription, resourceName, region)
     }
 
     return (
         <main>
             <h1 className="main--title">Resource Details</h1>
-            <p className="hero--text">Secure Design Portal is a tool to help internal customers to deploy secured infrastructures like Azure Key Vault and Azure Storage Account  through a self-service portal. </p>
+            <p className="hero--text">Secure Design Portal is a tool to help internal customers to deploy secured infrastructures like Azure Key Vault and Azure Storage Account. </p>
 
             <div className="form">
                 <label for="fname">Tenant ID</label>
@@ -24,21 +28,21 @@ export default function Main() {
                 <br />
 
                 <label for="subscription">Subscription   </label>
-                <select name="subscription">
+                <select name="subscription" onChange={(e)=>{setSubscription(e.target.value)}}>
                     <option selected disabled>Choose one</option>
-                    <option value="sydney">Sub 1</option>
-                    <option value="melbourne">Sub 2</option>
+                    <option value="sub1">Sub 1</option>
+                    <option value="sub2">Sub 2</option>
                 </select>
 
                 <br />
                 <br />
 
                 <label for="resourceGroup">Resource group     </label>
-                <select name="resourceGroup" id="jack">
+                <select name="resourceGroup" id="jack" onChange={(e)=>{setRG(e.target.value)}}>
                     <option selected disabled>Choose one</option>
-                    <option value="sydney">Resource Group 1</option>
-                    <option value="melbourne">My RG</option>
-                    <option value="cromwell">Pretty Resource Group</option>
+                    <option value="r1">Resource Group 1</option>
+                    <option value="r2">My RG</option>
+                    <option value="r3">Pretty Resource Group</option>
                 </select>
 
                 <br />
@@ -48,13 +52,13 @@ export default function Main() {
                 <label for="resourceGroup">Resource Type   </label>
                 <div className="radio">
                     <label>
-                        <input type="radio" value="azure-storage-account" />
+                        <input type="radio" value="asa" checked={resourceType === "asa"} onChange={(e)=>{setType(e.target.value)}}/>
                         Azure Storage Account
                     </label>
                 </div>
                 <div className="radio">
                     <label>
-                        <input type="radio" value="akv" />
+                        <input type="radio" value="akv" checked={resourceType === "akv"} onChange={(e)=>{setType(e.target.value)}}/>
                         Azure Key Vault
                     </label>
                 </div>
@@ -63,13 +67,13 @@ export default function Main() {
                 <br />
 
                 <label for="rname">Resource Name</label>
-                <input type="text" id="rname" name="rname" value="" />
+                <input type="text" id="rname" name="rname" placeholder="Resource Name"  onChange={(e)=>{setName(e.target.value)}}></input>
 
                 <br />
                 <br />
 
                 <label for="region">Region</label>
-                <select name="region" id="regionSelect">
+                <select name="region" id="regionSelect" onChange={(e)=>{setRegion(e.target.value)}}>
                     <option selected disabled>Choose one</option>
                     <option value="sydney">west us</option>
                     <option value="melbourne">east US</option>
