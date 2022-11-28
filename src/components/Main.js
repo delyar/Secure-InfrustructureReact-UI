f0b75beb529ed02a8d579e8110d07e51282c5827
Delyar Tabatabai <t-dtabatabai@microsoft.com>
2022-11-28 10:35:36 -0800

    etching data from API and showing in the select-options

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 export default function Main() {
 
@@ -9,12 +9,23 @@ export default function Main() {
     const [resourceType, setType] = useState("asa");
     const [resourceName, setName] = useState();
     const [region, setRegion] = useState();
+    const [subscriptionNames, setSubscriptions] = useState([]);
 
     function submitPressed() {
 
         console.log(tenantID, resourceGroup, subscription, resourceName, region)
     }
 
+    useEffect(() => {
+        fetch("https://swapi.dev/api/people")
+            .then((data) => data.json())
+            .then((val) => setSubscriptions(val["results"]))
+    }, [])
+
+    console.log(subscriptionNames, "subs")
+
+
+    // console.log(values, "values")
 
     return (
         <main>
@@ -22,25 +33,26 @@ export default function Main() {
             <p className="hero--text">Secure Design Portal is a tool to help internal customers to deploy secured infrastructures like Azure Key Vault and Azure Storage Account. </p>
 
             <div className="form">
-                <label for="fname">Tenant ID</label>
+                <label htmlFor="fname">Tenant ID</label>
                 <input disabled="disabled" type="text" id="fname" name="fname" value={tenantID} />
                 <br />
                 <br />
 
 
-                <label for="subscription">Subscription   </label>
-                <select name="subscription" onChange={(e)=>{setSubscription(e.target.value)}}>
-                    <option selected disabled>Choose one</option>
-                    <option value="sub1">Sub 1</option>
-                    <option value="sub2">Sub 2</option>
+                <label htmlFor="subscription">Subscription   </label>
+                <select name="subscription" onChange={(e) => { setSubscription(e.target.value) }}>
+                    <option value="DEFAULT" disabled>Choose one</option>
+                    {
+                        subscriptionNames.map((opts, i) => <option key={i} value={opts.name}>{opts.name}</option>)
+                    }
                 </select>
                 <br />
                 <br />
 
 
-                <label for="resourceGroup">Resource group     </label>
-                <select name="resourceGroup" id="jack" onChange={(e)=>{setRG(e.target.value)}}>
-                    <option selected disabled>Choose one</option>
+                <label htmlFor="resourceGroup">Resource group     </label>
+                <select name="resourceGroup" id="jack" onChange={(e) => { setRG(e.target.value) }}>
+                    <option value="DEFAULT" disabled>Choose one</option>
                     <option value="r1">Resource Group 1</option>
                     <option value="r2">My RG</option>
                     <option value="r3">Pretty Resource Group</option>
@@ -50,16 +62,16 @@ export default function Main() {
                 <br />
 
 
-                <label for="resourceGroup">Resource Type   </label>
+                <label htmlFor="resourceGroup">Resource Type   </label>
                 <div className="radio">
                     <label>
-                        <input type="radio" value="asa" checked={resourceType === "asa"} onChange={(e)=>{setType(e.target.value)}}/>
+                        <input type="radio" value="asa" checked={resourceType === "asa"} onChange={(e) => { setType(e.target.value) }} />
                         Azure Storage Account
                     </label>
                 </div>
                 <div className="radio">
                     <label>
-                        <input type="radio" value="akv" checked={resourceType === "akv"} onChange={(e)=>{setType(e.target.value)}}/>
+                        <input type="radio" value="akv" checked={resourceType === "akv"} onChange={(e) => { setType(e.target.value) }} />
                         Azure Key Vault
                     </label>
                 </div>
@@ -67,15 +79,15 @@ export default function Main() {
                 <br />
                 <br />
 
-                <label for="rname">Resource Name</label>
-                <input type="text" id="rname" name="rname" placeholder="Resource Name"  onChange={(e)=>{setName(e.target.value)}}></input>
+                <label htmlFor="rname">Resource Name</label>
+                <input type="text" id="rname" name="rname" placeholder="Resource Name" onChange={(e) => { setName(e.target.value) }}></input>
 
                 <br />
                 <br />
 
-                <label for="region">Region</label>
-                <select name="region" id="regionSelect" onChange={(e)=>{setRegion(e.target.value)}}>
-                    <option selected disabled>Choose one</option>
+                <label htmlFor="region">Region</label>
+                <select name="region" id="regionSelect" onChange={(e) => { setRegion(e.target.value) }}>
+                    <option value="DEFAULT" disabled>Choose one</option>
                     <option value="sydney">west us</option>
                     <option value="melbourne">east US</option>
                     <option value="cromwell">Europe</option>
